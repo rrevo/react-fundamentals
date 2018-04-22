30a3a1267a0e76ddfefbdeeb81e3992a1ed16fa6
Rahul Revo <rahulrevo@gmail.com>
2018-04-21 22:52:18 -0700

    Simplified BookList component

--- src/BookList.js
+++ src/BookList.js
@@ -1,35 +1,29 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import Book from './Book'
 
-class BookList extends Component {
-    render() {
-        const {title, books, format} = this.props;
-
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">
-                    {this.props.format === "single" && <Link to="/">All</Link>}
-                    {this.props.format === "single" && <span> &gt; </span>}
-                    {title}
-                </h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books.map((book) => (
-                            <li key={book.id}>
-                                <Book data={book}
-                                      format={format}
-                                      onUpdateShelf={this.props.onUpdateShelf}
-                                      searchContext={this.props.searchContext}/>
-                            </li>
-                        ))}
-                    </ol>
-                </div>
-            </div>
-        )
-    }
-}
+const BookList = ({title, books, format, onUpdateShelf, searchContext}) => (
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">
+            {format === "single" && <Link to="/">All</Link>}
+            {format === "single" && <span> &gt; </span>}
+            {title}
+        </h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+                {books.map((book) => (
+                    <li key={book.id}>
+                        <Book data={book}
+                              format={format}
+                              onUpdateShelf={onUpdateShelf}
+                              searchContext={searchContext}/>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    </div>
+);
 
 BookList.propTypes = {
     onUpdateShelf: PropTypes.func.isRequired
